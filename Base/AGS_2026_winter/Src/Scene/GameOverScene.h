8d4b8445ab64cc0e86200735f8da45c4c21c64f1
M4k1 Sr <shimurushimuru81@gmail.com>
2025-12-10 15:16:44 +0900

    ゲームオーバーシーンありました

---
+++ Base/AGS_2026_winter/Src/Scene/GameOverScene.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "SceneBase.h"
+
+class GameOver : public SceneBase
+{
+public:
+	// コンストラクタ
+	GameOver(void);
+	// デストラクタ
+	~GameOver(void);
+
+	void Init(void) override;
+	void Update(void) override;
+	void Draw(void) override;
+	void Release(void) override;
+
+private:
+
+	// ゲームオーバー画像
+	int imgGameOverScene_;
+
+};
